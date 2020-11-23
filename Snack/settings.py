cbeb6efe3c86e6ed9bbd35d2931dd9a1b3e305a7
georgijkarateev <Georgy.karat@gmail.com>
2020-11-23 15:46:51 +0300

    Static for home moved

--- Snack/settings.py
+++ Snack/settings.py
@@ -123,8 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 LOGIN_REDIRECT_URL = '/feed'
 
