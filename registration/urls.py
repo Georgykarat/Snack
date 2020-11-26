2e7bb2dbb463bad7966b2be87edd49bfb89d700e
georgijkarateev <Georgy.karat@gmail.com>
2020-11-26 21:17:55 +0300

    AJAX11

--- registration/urls.py
+++ registration/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .import views
+from registration.views import regcode
 
 urlpatterns = [
     path("", views.registration, name="registration_page"),
-    path("regcode/", views.regcode, name="regcode")
+    path("regcode/", regcode, name="regcode"),
 ]
