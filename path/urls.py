4e89260406d71b9c4eb92c429d34cdc667b794f3
georgijkarateev <Georgy.karat@gmail.com>
2020-12-06 18:13:12 +0300

    links fixed

--- path/urls.py
+++ path/urls.py
@@ -3,6 +3,6 @@ from .import views
 
 urlpatterns = [
     path("", views.path, name="path_page"),
-    path("1/", views.python_base, name="python_basics"),
-    path("1/1/", views.lesson_data, {'lesson':'002'}, name="python_basics_1")
+    path("0001/", views.python_base, name="python_basics"),
+    path("0001/01/", views.lesson_data, {'lesson':'000101'}, name="python_basics_1")
 ]
