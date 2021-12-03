2982a11946e26b514e8abb2519c6bb5ae8c3e040
georgijkarateev <Georgy.karat@gmail.com>
2021-12-03 23:30:20 +0300

    redirect if authorized from login to feed

--- login/views.py
+++ login/views.py
@@ -7,27 +7,30 @@ from django.contrib.auth.views import LoginView
 # Create your views here.
 
 
-def login_view(request):
-    if request.method == 'POST':
-        auth_form = AuthForm(request.POST)
-        if auth_form.is_valid():
-            mail = auth_form.cleaned_data['mail']
-            password = auth_form.cleaned_data['password']
-            user = authenticate(username=mail.lower(), password=password)
-            if user:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponseRedirect('../feed')
+def login_view(request, *args, **kwargs):
+    if request.user.is_authenticated == True:
+        return HttpResponseRedirect('../feed')
+    else:    
+        if request.method == 'POST':
+            auth_form = AuthForm(request.POST)
+            if auth_form.is_valid():
+                mail = auth_form.cleaned_data['mail']
+                password = auth_form.cleaned_data['password']
+                user = authenticate(username=mail.lower(), password=password)
+                if user:
+                    if user.is_active:
+                        login(request, user)
+                        return HttpResponseRedirect('../feed')
+                    else:
+                        auth_form.add_error('__all__', 'Erroe! An account is inactive!')
                 else:
-                    auth_form.add_error('__all__', 'Erroe! An account is inactive!')
-            else:
-                auth_form.add_error('__all__', 'Login or password incorrect')
-    else:
-        auth_form = AuthForm()
-    context = {
-        'form': auth_form
-    }
-    return render(request, 'login/login.html', context=context)
+                    auth_form.add_error('__all__', 'Login or password incorrect')
+        else:
+            auth_form = AuthForm()
+        context = {
+            'form': auth_form
+        }
+        return render(request, 'login/login.html', context=context)
 
 
 class MainLoginView(LoginView):
